8c53155c400e3d0e596d62b3b4585d5b198cfe4a
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2017-06-20 16:36:41 -0400

    Remove semicolons & add `_all.js` to paths
    Close #90

--- spec/_all.js
+++ spec/_all.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
 // Load all specs so webpack can find them. Think of this as an automatic
 // manifest for bundling specs.
-const req = require.context('./', true, /spec\.js$/);
-req.keys().forEach(req);
+const req = require.context('./', true, /spec\.js$/)
+req.keys().forEach(req)
