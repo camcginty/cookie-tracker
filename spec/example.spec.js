d60d2c410ab71cbaddef98089ee5e3da62682016
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-08-09 10:49:40 -0400

    Switch tests to use mocha/chai instead of jasmine
    To be more consistent with `node_template`. Close #40.

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -1,9 +1,10 @@
 'use strict'
 
+const expect = require('chai').expect
 const example = require('../assets/scripts/example')
 
 describe('Example', function () {
   it('is true', function () {
-    expect(example).toBe(true)
+    expect(example).to.be.true
   })
 })
