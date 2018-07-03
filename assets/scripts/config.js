fafbc9397bc7d3f0c1a941b88f3e35cb04438bd6
punzie <c_a_mcginty@yahoo.com>
2018-07-03 17:09:25 -0400

    edit url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://cookie-tracker.herokuapp.com/',
+  production: 'https://cookie-tracker.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
